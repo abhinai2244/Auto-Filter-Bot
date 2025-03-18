69eed16675dbbdb065e94f573387dc958aa9fb6a
Abhinai Reddy⛔️ <159715958+abhinai2244@users.noreply.github.com>
2025-03-18 12:55:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -117,7 +117,7 @@ SHORTLINK = is_enabled('SHORTLINK', False)
 
 # for stream
 IS_STREAM = is_enabled('IS_STREAM', False)
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "")
+BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002268831006")
 if len(BIN_CHANNEL) == 0:
     print('Error - BIN_CHANNEL is missing, exiting now')
     exit()
